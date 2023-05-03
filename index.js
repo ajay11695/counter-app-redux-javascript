9ea338df9782fddb81ac875499a271b1b84619e4
ajay <ajayrajput9554@gmail.com>
2023-05-03 20:32:16 +0530

    counter-app with redux+js

---
+++ index.js
@@ -0,0 +1,89 @@
+let store = Redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
+
+let steps = document.querySelector('.steps')
+let max_value = document.querySelector('.max_value')
+let increment = document.querySelector('#increment')
+let decrement = document.querySelector('#decrement')
+let reset = document.querySelector('#reset')
+let h1 = document.querySelector('h1')
+
+
+let step = 5
+let max = 15
+let Arr = [5, 10, 15, 15, 100, 200]
+function btnUI(){
+    steps.innerHTML=''
+    max_value.innerHTML=''
+    Arr.forEach((element, i) => {
+        if (i > 2) {
+            let maxBtn = document.createElement('p')
+            maxBtn.innerText = element
+            maxBtn.className = element === max ? 'btn active' : 'btn'
+            maxBtn.addEventListener('click',()=>{
+                max=element
+                btnUI()
+            })
+            max_value.append(maxBtn)
+        } else {
+            let stepBtn = document.createElement('p')
+            stepBtn.innerText = element
+            stepBtn.className = element === step ? 'btn active' : 'btn'
+            stepBtn.addEventListener('click',()=>{
+                step=element
+                btnUI()
+            })
+            steps.append(stepBtn)
+        }
+    });    
+}
+btnUI()
+
+let counter=store.getState()
+h1.innerText=counter
+
+increment.addEventListener("click", () => {
+    store.dispatch({
+        type: 'increment',
+        step,
+        max
+    })
+
+});
+decrement.addEventListener("click", () => {
+    store.dispatch({
+        type: 'decrement',
+        step,
+        max
+    })
+
+});
+reset.addEventListener("click", () => {
+    store.dispatch({
+        type: 'reset'
+    })
+
+});
+
+store.subscribe(()=>{
+    counter=store.getState()
+    h1.innerText=counter
+})
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case "increment":
+            return state<action.max?state+action.step:state;
+
+        case "decrement":
+            return state>0?state - action.step:state;
+
+        case "reset":
+            return 0;
+
+        default:
+            return state;
+    }
+}
+
+
+
---
+++ todo.md
@@ -0,0 +1,43 @@
+## Counter With Redux
+
+# Redux Counter App
+
+- Create a counter app using Redux and JavaScript
+- It will have a counter value initialized with 0
+- It should have three buttons Increment, Decrement and Reset
+- When you click on the Increment it will add one to counter
+- When you click on the Decrement it will subtract one from counter
+- When you click on the Reset it will reset the counter to 0
+
+You have to make three application one after another. Don't change this app to add the functionality of Extension I and II. Make them separately one after another. You can copy the css/html.
+
+# Extension I
+
+- Create a counter application with the initial counter value to be 0
+- It will have a steps functionality
+- User will first select the steps either 5, 10 or 15.
+- If user does not select the steps it should default to 1
+- Once step is selected and user clicks on Increment the values of counter will change by step.
+- Clicking on Decrement will decrease the value by step
+- Clicking on Reset will reset the counter value to 0
+
+# Example:
+
+- If user selects 5 as step
+- Counter will start from 0
+- Clicking on Increment button will change counter to 5
+- Clicking on Increment button again will change counter to 10
+- Clicking on Decrement button will change counter to 5
+- Clicking on Reset button will change the counter value to 0
+- If step is not selected increment and decrement by 1
+
+# Extension II
+
+In this extension we will add a new feature to our counter application. With this new feature will have a max value. Once `max` value is selected the value of counter will not exceed. By default it should be infinity.
+
+For Example:
+
+If user selected the `step` to be `5` and `max` to `15`. The value of counter should increment/decrement by 5 and should not exceed the `max` value.
+
+# App UI With Steps
+![App UI]("https://github.com/s07/TA-FRONTEND-state-and-forms-TRaaab/raw/block-BRaabf/block-BRaabf/assets/max.png")
